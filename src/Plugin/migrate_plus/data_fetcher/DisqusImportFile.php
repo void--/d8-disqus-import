d43e77b56f7a0ffcd335a289be8521f66a5c8cdc
Theo Ballew <theoballew@gmail.com>
2018-02-20 19:02:52 -0800

    add custom data fetcher plugin

---
+++ src/Plugin/migrate_plus/data_fetcher/DisqusImportFile.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\disqus_import\Plugin\migrate_plus\data_fetcher;
+
+use Drupal\migrate_plus\Plugin\migrate_plus\data_fetcher\File;
+/**
+ * Retrieve data from a Disqus Json dump.
+ *
+ * @DataFetcher(
+ *   id = "disqus_import_file",
+ *   title = @Translation("Disqus Import File")
+ * )
+ */
+class DisqusImportFile extends File {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getResponse($url) {
+    // $url is unused.
+    $disqus_dump_path = drupal_get_path('module', 'disqus_import') . '/disqus_data.json';
+    $response = file_get_contents($disqus_dump_path);
+    if ($response === FALSE) {
+      throw new MigrateException('file parser plugin: could not retrieve data from ' . $disqus_dump_path);
+    }
+    return $response;
+  }
+
+}
