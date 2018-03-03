9066c7195ada133cd69f7274aa803d56d02744fb
Theo Ballew <theoballew@gmail.com>
2018-03-02 19:56:25 -0800

    flesh out custom migrate process callbacks

--- src/DisqusImportMigrateProcess.php
+++ src/DisqusImportMigrateProcess.php
@@ -8,14 +8,19 @@ class DisqusImportMigrateProcess {
     $isDeleted = $args[0];
     $isSpam =    $args[1];
 
-    return (!$isDeleted && !$isSpam) ? 1 : 0;
+    return (($isDeleted === 'false') && ($isSpam === 'false')) ? 1 : 0;
   }
 
   public static function getPid($parent) {
-    $test = 'blah';
+    // Grab the cid from the key value map created on entity save.
+    // @see \Drupal\disqus_import\Plugin\migrate\destination\DisqusImportEntityComment::save
+    $pid = \Drupal::keyValue('disqus_import')->get($parent);
+    return $pid ? $pid : 0;
   }
 
   public static function getUidFromEmail($email) {
-    $test = 'blah';
+    $user = user_load_by_mail($email);
+
+    return $user ? $user->id() : null;
   }
 }
